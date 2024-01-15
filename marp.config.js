f6e75d114b0553fc4241c9f4617efcf2a1071c77
h5wawaji <306612345@qq.com>
2024-01-15 22:36:03 +0800

    0115 update

--- marp.config.js
+++ marp.config.js
@@ -1,6 +1,15 @@
+const canonicalUrl = process.env.URL || undefined
+const ogImage = (() => {
+  if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}/og-image.jpg`
+
+  return undefined
+})()
+
 module.exports = {
   allowLocalFiles: true,
-  ogImage: process.env.URL && `${process.env.URL}/og-image.jpg`,
+  ogImage,
   themeSet: 'themes',
-  url: process.env.URL,
+  url: canonicalUrl,
 }
